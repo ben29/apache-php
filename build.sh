08cc379881d79a1368e2568222383ade87334b0c
Ben Hakim <ben@hakim.co.il>
2024-10-30 03:23:38 +0200

    fix Dockerfile

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-
 git pull
+docker rmi -f $(docker images -q)
 docker build -t php-apache:1.0 .
 docker tag php-apache:1.0 benhakim2010/php-apache:1.0
 docker tag php-apache:1.0 benhakim2010/php-apache:latest
