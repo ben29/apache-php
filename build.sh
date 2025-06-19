d73365e7abf3fbd0a692be9e0b744ccda1d579f0
Ben Hakim <ben@hakim.co.il>
2025-06-19 23:14:04 +0300

    fixes

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ APP_VERSION="2.0"
 TAG="benhakim2010/apache-php:${APP_VERSION}"
 
 docker build --progress=plain --no-cache --build-arg APP_VERSION=${APP_VERSION} -t "${TAG}" .
-#docker push "${TAG}"
+docker push "${TAG}"
