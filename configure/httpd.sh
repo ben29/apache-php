5a78ba92c563c82981a41d9339d92b987d8fe665
Ben Hakim <ben@hakim.co.il>
2025-06-19 13:29:23 +0300

    fixes

--- configure/httpd.sh
+++ configure/httpd.sh
@@ -1,31 +1,79 @@
+#!/usr/bin/env bash
+
 ./configure \
-    --prefix=/etc/httpd \
+		--prefix=/etc/httpd \
+    --bindir=/usr/local/bin \
+    --sbindir=/usr/local/bin \
+    --datadir=/var/www \
+    --mandir=/var/www \
     --with-mpm=event \
-    --enable-unixd \
-    --enable-authz-core \
-    --enable-dir \
-    --enable-mime \
-    --enable-logio \
-    --enable-headers \
-    --enable-setenvif \
-    --enable-env \
-    --enable-http2 \
-    --enable-rewrite \
-    --enable-ssl \
-    --enable-expires \
+    --enable-deflate \
     --enable-unique-id \
-    --enable-status \
-    --disable-so \
-    --disable-cgi \
-    --disable-dav \
-    --disable-userdir \
-    --disable-autoindex \
-    --disable-proxy \
+    --enable-mods-static=all \
+    --enable-logio \
+    --enable-ssl \
+    --enable-rewrite \
+    --enable-expires \
+    --enable-reqtimeout \
+    --enable-headers \
+    --enable-http2 \
+    --enable-allowmethods \
+    --enable-proxy-fcgi \
+    --enable-so \
+    --disable-actions \
+    --disable-authn-socache \
+    --disable-file-cache \
     --disable-cache \
-    --disable-auth-basic \
-    --disable-auth-digest \
+    --disable-cache-disk \
+    --disable-cache-socache \
+    --disable-socache-dbm \
+    --disable-socache-memcache \
+    --disable-socache-redis \
+    --disable-socache-dc \
+    --disable-md \
+    --disable-buffer \
+    --disable-userdir \
+    --disable-status \
+    --disable-dav \
+    --disable-autoindex \
+    --disable-cgi \
+    --disable-cgid \
+    --disable-info \
+    --disable-sed \
+    --disable-version \
     --disable-auth-form \
+    --disable-auth-digest \
+    --disable-auth-basic \
     --disable-authn-core \
     --disable-authn-file \
+    --disable-authn-dbm \
+    --disable-authn-dbd \
+    --disable-authn-anon \
+    --disable-authz-groupfile \
     --disable-authz-user \
-    --disable-authz-groupfile
+    --disable-authz-dbm \
+    --disable-authz-owner \
+    --disable-authz-dbd \
+    --disable-authn-socache \
+    --disable-watchdog \
+    --disable-access-compat \
+    --disable-macro \
+    --disable-dbd \
+    --disable-ext-filter \
+    --disable-session-dbd \
+    --disable-suexec \
+    --disable-substitute \
+    --disable-log-debug \
+    --disable-speling \
+    --disable-proxy-html \
+    --disable-proxy-connect \
+    --disable-proxy-ftp \
+    --disable-proxy-http \
+    --disable-proxy-scgi \
+    --disable-proxy-uwsgi \
+    --disable-proxy-ajp \
+    --disable-proxy-balancer \
+    --disable-proxy-express \
+    --disable-proxy-wstunnel \
+    --disable-proxy-fdpass \
+    --disable-vhost-alias
