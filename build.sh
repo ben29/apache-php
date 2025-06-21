19e7009dc4d76045f5b0876ad969f0a7a8e47645
Ben Hakim <ben@hakim.co.il>
2025-06-22 01:26:51 +0300

    try 1

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -e
 APP_VERSION="1.0"
 TAG="benhakim2010/apache-php:${APP_VERSION}"
 
-docker build --progress=plain --no-cache --build-arg APP_VERSION=${APP_VERSION} -t "${TAG}" .
+docker build --progress=plain --no-cache -t "${TAG}" .
 docker push "${TAG}"
